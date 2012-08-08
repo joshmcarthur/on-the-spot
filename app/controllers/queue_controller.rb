5f90904ccd02ae4b2528aa5108db06f9baa0116b
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-08 23:34:22 +1200

    Add index and create actions to show tracks, and add a track to the queue

--- app/controllers/queue_controller.rb
+++ app/controllers/queue_controller.rb
@@ -1,5 +1,23 @@
 class QueueController < ApplicationController
   def index
+    @current = MetaSpotify::Track.lookup($redis.get("currently_playing")) rescue nil
+    @next    = []
 
+    (-3..-1).to_a.each do |index|
+      track_uri = $redis.lindex("tracks", index)
+      next unless track_uri
+      @next << Rails.cache.fetch(track_uri) do
+        MetaSpotify::Track.lookup(track_uri)
+      end
+    end
+
+    @next.reverse!
+    
+    render
+  end
+
+  def create
+    $redis.lpush "tracks", params[:uri]
+    render :nothing => true, :status => :created
   end
 end
