93ce3698c4eee0540782fa26dfd8d262d9179450
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-09 19:57:42 +1200

    Add Spotify controller daemon

---
+++ lib/daemons/spotify_controller.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# You might want to change this
+ENV["RAILS_ENV"] ||= "production"
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
+
+$running = true
+Signal.trap("TERM") do 
+  $running = false
+end
+
+while($running) do
+  
+  # Replace this with your code
+  Rails.logger.auto_flushing = true
+  Rails.logger.info "This daemon is still running at #{Time.now}.\n"
+
+  $player = Hallon::Player.new(Hallon::OpenAL)
+
+  while track = $redis.lpop("tracks")
+    $player.play!(track)
+  end
+  
+  sleep 10
+end
