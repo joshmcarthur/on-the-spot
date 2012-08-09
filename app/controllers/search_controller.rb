9e784805a169ee62f108a9a25cd7fcb2ef066566
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-09 21:52:43 +1200

    Use Hallon to do the search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,18 @@
 class SearchController < ApplicationController
 
-
   def index
     render
   end
 
-
   def new
     @results = Rails.cache.fetch "search-for-#{params[:q]}" do
-      Hallon::Search.new(params[:q], :tracks => 25)
+      Hallon::Search.new(params[:q], :tracks => 25, :tracks_offset => 10).load.tracks.to_a.map do |track|
+        {:name => track.name, :popularity => track.popularity, :uri => track.to_link.to_str}
+      end
     end
 
     respond_to do |format|
-      format.json { render :json => @results[:tracks][0..25] }
+      format.json { render :json => @results }
     end
   end
 end
