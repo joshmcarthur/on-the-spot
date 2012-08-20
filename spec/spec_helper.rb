c93926d140a2145f1509b6e935a67e7152597c70
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-20 13:08:44 +1200

    Stub out just enough of Hallon to get passing tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,12 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.before(:each) do
+    $player.stub(:play!)
+    $player.stub(:load)
+    Hallon::Track.any_instance.stub(:load).and_return(OpenStruct.new({:name => "Hurt Feelings"}))
+  end
+
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
