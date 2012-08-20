6d7716401e68f8a00690832494a9cef1c8ccac88
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-20 13:08:30 +1200

    Do not attempt to log into Spotify if we are running tests

--- config/initializers/hallon.rb
+++ config/initializers/hallon.rb
@@ -1,5 +1,5 @@
 $hallon_session = Hallon::Session.initialize IO.read(Rails.root.join('config', 'keys', 'spotify_appkey.key'))
-$hallon_session.login! ENV['SPOTIFY_USERNAME'], ENV['SPOTIFY_PASSWORD']
+$hallon_session.login!(ENV['SPOTIFY_USERNAME'], ENV['SPOTIFY_PASSWORD']) unless Rails.env.test?
 
 $player = Hallon::Player.new(Hallon::OpenAL)
 $player.volume_normalization = true
