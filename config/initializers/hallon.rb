d76527ed293aaa794b50540c2baa260cd3b3e2f3
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-21 08:57:15 +1200

    Do not log in if running in the test environment

--- config/initializers/hallon.rb
+++ config/initializers/hallon.rb
@@ -1,5 +1,7 @@
-$hallon_session = Hallon::Session.initialize IO.read(Rails.root.join('config', 'keys', 'spotify_appkey.key'))
-$hallon_session.login!(ENV['SPOTIFY_USERNAME'], ENV['SPOTIFY_PASSWORD']) unless Rails.env.test?
 
+$hallon_session = Hallon::Session.initialize IO.read(Rails.root.join('config', 'keys', 'spotify_appkey.key'))
+$hallon_session.login!(ENV['SPOTIFY_USERNAME'] || "test", ENV['SPOTIFY_PASSWORD'] || "password") unless Rails.env.test?
 $player = Hallon::Player.new(Hallon::OpenAL)
 $player.volume_normalization = true
+
+
