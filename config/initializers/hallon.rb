d4c866246fc9d00d10c2e86549e235bce49ff8d6
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-21 22:35:44 +1200

    Moved Spotify setup to application.rb, now just call this method when the application boots

--- config/initializers/hallon.rb
+++ config/initializers/hallon.rb
@@ -1,7 +1,3 @@
-
-$hallon_session = Hallon::Session.initialize IO.read(Rails.root.join('config', 'keys', 'spotify_appkey.key'))
-$hallon_session.login!(ENV['SPOTIFY_USERNAME'] || "test", ENV['SPOTIFY_PASSWORD'] || "password") unless Rails.env.test?
-$player = Hallon::Player.new(Hallon::OpenAL)
-$player.volume_normalization = true
-
-
+unless Rails.env.test?
+  OnTheSpot::Application.setup_spotify!
+end
