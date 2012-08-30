ff38ace3a90893b091cc8f2fd8c177e1a8b00b8a
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-30 20:29:49 +1200

    Metaprogramme a 'uri' accessor onto Hallon::Track

--- config/initializers/hallon.rb
+++ config/initializers/hallon.rb
@@ -1,3 +1,11 @@
-unless Rails.env.test?
-  OnTheSpot::Application.setup_spotify!
+OnTheSpot::Application.setup_spotify!
+
+# We use the Spotify URI quite a bit
+# define a URI method on Hallon Track
+# to make this a bit neater
+Hallon::Track.class_eval do
+  def uri
+    self.to_link.to_str
+  end
 end
+
