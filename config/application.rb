c66630db96a083c8e7794615473e04a56d3d658e
Josh McArthur <joshua.mcarthur@gmail.com>
2012-10-26 08:37:53 +1300

    Compile any asset files in app/assets/static

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module OnTheSpot
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.paths += %w( static )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
