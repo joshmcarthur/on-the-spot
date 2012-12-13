760fd3f7585719b1e6d5ca045b3656c0ddf1b7aa
Josh McArthur <joshua.mcarthur@gmail.com>
2012-12-13 14:26:36 +1300

    Add initializer to broadcast application on Bonjour

---
+++ config/initializers/bonjour_broadcast.rb
@@ -0,0 +1,4 @@
+text_record = DNSSD::TextRecord.new
+DNSSD.register("On the Spot", "_http._tcp", "local", 3000, text_recordg.encode) do |reply|
+  Rails.logger.info "Announcing On the Spot on Bonjour..."
+end
