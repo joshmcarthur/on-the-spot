b558b9351faa5888e1a49b8dc72ff3b961468214
Josh McArthur <joshua.mcarthur@gmail.com>
2012-12-13 14:54:09 +1300

    Fix wrong character

--- config/initializers/bonjour_broadcast.rb
+++ config/initializers/bonjour_broadcast.rb
@@ -1,4 +1,4 @@
 text_record = DNSSD::TextRecord.new
-DNSSD.register("On the Spot", "_http._tcp", "local", 3000, text_recordg.encode) do |reply|
+DNSSD.register("On the Spot", "_http._tcp", "local", 3000, text_record.encode) do |reply|
   Rails.logger.info "Announcing On the Spot on Bonjour..."
 end
